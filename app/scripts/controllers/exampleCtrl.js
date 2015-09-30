f4932cbe5c3b7a9589de788c44ec7fec6d08845d
Evelyne Wauters <evelyne.wauters@gmail.com>
2015-09-30 12:00:39 +0200

    added ng-repeat and ng-hide

--- app/scripts/controllers/exampleCtrl.js
+++ app/scripts/controllers/exampleCtrl.js
@@ -6,10 +6,18 @@
   function ExampleCtrl($scope)  {
     console.log('create exampleCtrl');
 
+    $scope.data = [
+      {prop:'a'},
+      {prop:'b'},
+      {prop:'c'},
+      {prop:'d'},
+      {prop:'e'}];
+
     $scope.$on('userclicked', function(event, data) {
       console.log(data);
       console.log(event);
       $scope.message = data.number;
-    })
+    });
+
   }
 })();
