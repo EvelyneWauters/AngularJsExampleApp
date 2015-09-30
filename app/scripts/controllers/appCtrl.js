f826ddc98f6ab2551f9fc8f006c915dde6f5e94b
Evelyne Wauters <evelyne.wauters@gmail.com>
2015-09-30 11:32:02 +0200

    made appCtrl scopeless

--- app/scripts/controllers/appCtrl.js
+++ app/scripts/controllers/appCtrl.js
@@ -2,18 +2,9 @@
   angular.module('exampleApp.controllers')
     .controller('appCtrl', AppCtrl);        //hier geen array meegeven of hij zou opnieuw gemaakt worden
 
-  function AppCtrl($scope, appTitle)  {
-    $scope.title = appTitle;                //deze var is nu op de scope aanwezig onder de naam 'title'
+  function AppCtrl(appTitle)  {
+    this.title = appTitle;                //deze var is nu op de scope aanwezig onder de naam 'title'
 
 
-    $scope.sendEvent = function() {
-      $scope.$broadcast('userclicked', {number : 123});   //object meegeven om later te tonen dat we daar nog aankunnen
-    }
-
-    $scope.$watch('title', function(newValue, oldValue)  {
-      console.log('old: '+ oldValue);
-      console.log('new: '+ newValue);
-
-    });
   }
 })();
