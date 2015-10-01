4faaea94307bd67d8df1737fe7a9f5f2a1e3d0c1
Evelyne Wauters <evelyne.wauters@gmail.com>
2015-10-01 12:25:43 +0200

    ngannotate (in de build taak) handmatig implementeren

--- app/scripts/controllers/appCtrl.js
+++ app/scripts/controllers/appCtrl.js
@@ -1,8 +1,16 @@
 (function() {
   angular.module('exampleApp.controllers')
-    .controller('appCtrl', AppCtrl);        //hier geen array meegeven of hij zou opnieuw gemaakt worden
+    .controller('appCtrl', ['appTitle', '$location', AppCtrl]);        //hier geen array meegeven of hij zou opnieuw gemaakt worden
 
-  function AppCtrl(appTitle, $location)  {
+
+  //uglyfication/minification gaat de namen van de variableen aanpassen naar 'a', 'b', waardoor we de services kwijt geaken
+  // maar past GEEN STRINGS AAN
+  //STAP 1. Array maken van AppCtrl functie die we meegeven aan de .controller
+  //STAP 2. de 'appTitl', '$location' hier meegeven als strings
+  //STAP3. aan de functie AppCtrl() kunnen nu random namen meegeven
+  //STAP4. gebruik de taak ngannotate en het werk out of the box (is automatisch)
+
+  function AppCtrl(a,n)  {
     this.title = appTitle;                //deze var is nu op de scope aanwezig onder de naam 'title'
 
 
